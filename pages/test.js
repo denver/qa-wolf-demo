48839f50795dbaecb687d62fa3ba0f97cd1a7cc1
denver <denver.peterson@gmail.com>
2020-09-09 00:15:36 -0700

    test page test

--- pages/test.js
+++ pages/test.js
@@ -1,26 +1,26 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Test() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create QA Wolf Demo</title>
+        <title>Test Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://qawolf.com">QA Wolf!</a>
+          Test Page
         </h1>
 
         <p className={styles.description}>
-          <code className={styles.code}><a href="/test">Test Page</a></code>
+          <code className={styles.code}><a href="/">Home</a></code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
+          <a href="/" className={styles.card}>
+            <h3>Home &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
